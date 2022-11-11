06f69189be6b69b06e2ef460d344d7eeb9ae9ab4
Arutoria <2304968145@qq.com>
2022-11-11 23:16:12 +0800

    HotFix第一次测试提交

--- src/test/java/com/itheima/GitTest.java
+++ src/test/java/com/itheima/GitTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 public class GitTest {
 
     @Test
-    public  void  gitTest(){
+    public void gitTest() {
 
         System.out.println("Git First Update !");
         System.out.println("Git Second Update !");
+        System.out.println("Git Third Update !");
 
+        System.out.println("hotfix First Update");
     }
 }
